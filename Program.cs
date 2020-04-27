f5c20c208b273b6461bf5b83e4b80f6e4647dcb3
chdmitr <chdmitr@skbkontur.ru>
2020-04-27 20:31:31 +0500

    init

--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using Npgsql;
 
 namespace Version
 {
@@ -7,37 +6,7 @@ namespace Version
     {
         static void Main(string[] args)
         {
-            var PosrgresUser = "superuser";
-            var PostgresPass = "SuperUser";
-            var PosgresDB    = "SuperDB";
-            var PostgresPort = "5432";
-
-            Console.WriteLine($"Hello from {Environment.MachineName}");
-
-            var cs = $"Host=MyServer;Username={PosrgresUser}:{PostgresPort};Password={PostgresPass};Database={PosgresDB}";
-
-            Console.WriteLine($"Try to connect to PostgreSQL with connection string:\n{cs}");
-
-            using var con = new NpgsqlConnection(cs);
-            try
-            {
-                con.Open();
-                var sql = "SELECT version()";
-                using var cmd = new NpgsqlCommand(sql, con);
-                var version = cmd.ExecuteScalar().ToString();
-                Console.WriteLine($"PostgreSQL version: {version}");
-            }
-            catch (Npgsql.NpgsqlException e)
-            {
-                Console.WriteLine($"Error!\n{e.InnerException}");
-                
-            }
-            finally
-            {
-                con.Close();
-            }
-
-            Console.ReadKey();            
+            throw new Exception("The main code in the test branch");
         }
     }
 }
